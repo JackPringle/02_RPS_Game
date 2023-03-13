4feb9c2695f5ba9331811c3323675a20e8215ea3
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-03-14 12:53:29 +1300

    Basic input + loop

--- 02_user_choice.py
+++ 02_user_choice.py
@@ -1,23 +1,35 @@
 # Functions go here
 def choice_checker(question):
+    error = "Please choose from rock / paper / scissors (or xxx to quit)"
 
     valid = False
     while not valid:
 
-        # Ask user for choice
-        response = input(question)
+        # Ask user for choice (and put choice in lowercase)
+        response = input(question).lower()
 
         if response == "r" or response == "rock":
             return response
+        elif response == "p" or response == "paper":
+            return response
+        elif response == "s" or response == "scissors":
+            return response
+
+        # check for exit code...
+        elif response == "xxx":
+            return response
+        else:
+            print(error)
 
 
 # Main routine goes here
 
-# Ask user for choice and check it's valid
-user_choice = choice_checker("Choose rock / paper / scissors (r/p/s): ")
 
-# Ask user for choice and check it's valid
+# Loop for testing purposes
+user_choice = ""
+while user_choice != "xxx":
+    # Ask user for choice and check it's valid
+    user_choice = choice_checker("Choose rock / paper / scissors (r/p/s): ")
 
-
-# print out choice for comparison purposes
-
+    # Ask user for choice and check it's valid
+    print(f"You chose {user_choice}")
