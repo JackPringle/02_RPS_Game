d979aa0856f297b4d3ffb7ac0cc5b48da92430cd
JackPringle <101750172+JackPringle@users.noreply.github.com>
2023-03-14 13:13:16 +1300

    A bit more efficient

---
+++ 02_user_choice_v2.py
@@ -0,0 +1,38 @@
+# Version 2 - error message included when calling function
+
+# Functions go here
+def choice_checker(question, error):
+
+    valid = False
+    while not valid:
+
+        # Ask user for choice (and put choice in lowercase)
+        response = input(question).lower()
+
+        if response == "r" or response == "rock":
+            return "rock"
+        elif response == "p" or response == "paper":
+            return "paper"
+        elif response == "s" or response == "scissors":
+            return "scissors"
+
+        # check for exit code...
+        elif response == "xxx":
+            return "xxx"
+        else:
+            print(error)
+
+
+# Main routine goes here
+
+
+# Loop for testing purposes
+user_choice = ""
+while user_choice != "xxx":
+
+    # Ask user for choice and check it's valid
+    user_choice = choice_checker("Choose rock / paper / scissors (r/p/s): ",
+                                 "Please choose from rock / paper / scissors (or xxx to quit)")
+
+    # print out choice for comparison purposes
+    print(f"You chose {user_choice}")
